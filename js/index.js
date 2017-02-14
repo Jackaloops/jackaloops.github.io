053810935c2aed355fdd9db182376d8d81383c73
Jack Shen <jack.shen97@gmail.com>
2017-02-14 01:44:39 -0500

    finalizing mobile front page

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
+// JS scripts for hover effects on my kickass lantern
+// Yeah, kinda inefficient because its loading mobile AND desktop eventListeners, but i don't know enough jQuery yet :I
+// Will fix eventually
+
+//finds variables
 var showButton = document.getElementById('showcase_button');
 console.log(showButton);
+var showButtonMobile = document.getElementById('toggle_button');
+console.log(showButtonMobile);
 
+//stores file path of showcase-icon.svg
+var mobileVer = showButtonMobile.getElementsByTagName('img')[0].src;
+
+//adds listeners
 showButton.addEventListener('mouseenter', changeShowPic);
-showButton.addEventListener('mouseleave', changeShowPicBack)
+showButton.addEventListener('mouseleave', changeShowPicBack);
+
+showButtonMobile.addEventListener('touchstart', changeMobile);
+
 
 function changeShowPic() {
 	// console.log('function called');
@@ -12,4 +26,16 @@ function changeShowPic() {
 function changeShowPicBack() {
 	// console.log('return function called');
 	showButton.getElementsByTagName('img')[0].src = 'img/showcase-icon.svg';
+}
+
+function changeMobile() {
+
+	// console.log('changeMobile triggered: ', showButtonMobile.getElementsByTagName('img')[0].src);
+
+	if(showButtonMobile.getElementsByTagName('img')[0].src === mobileVer) {
+		showButtonMobile.getElementsByTagName('img')[0].src = 'img/showcase-hover-icon.svg';
+	}
+	else {
+		showButtonMobile.getElementsByTagName('img')[0].src = 'img/showcase-icon.svg';
+	}
 }
